c46155a06e149fe271ba938917265441cd6cd430
Oscar González <oagmoa@gmail.com>
2017-07-07 19:43:25 -0600

    echoes

---
+++ ArbolAVL.py
@@ -0,0 +1,176 @@
+from graphviz import Digraph
+
+class NodoAvl(object):
+    def __init__(self,dato=None,padre=None):
+        self.dato=dato
+        self.balance=0
+        self.altura=0
+        self.derecha=None
+        self.izquierda=None
+        self.padre=padre
+
+class AVL(object):
+    def __init__(self,raiz=None):
+        self.raiz=raiz
+
+    def insertar(self,dato):
+        if self.raiz is None:
+            self.raiz = NodoAvl(dato)
+        else:
+            n = self.raiz
+            padre = NodoAvl()
+            while(True):
+                if(n.dato==dato):
+                    return False
+                padre = n
+                izquierda = n.dato > dato
+                n = n.izquierda if izquierda else n.derecha
+                if n is None:
+                    if izquierda:
+                        padre.izquierda = NodoAvl(dato,padre)
+                    else:
+                        padre.derecha = NodoAvl(dato,padre)
+                    self.rebalancear(padre)
+                    break
+        return True
+
+    def eliminarnodo(self,nodo):
+        if(nodo.izquierda is None and nodo.derecha is None):
+            if(nodo.padre is None): self.raiz=None
+            else:
+                padre = nodo.padre
+                if(padre.izquierda == nodo):
+                    padre.izquierda = None
+                else:
+                    padre.derecha = None
+                self.rebalancear(padre)
+            return
+        if nodo.izquierda is not None:
+            hijo = nodo.izquierda
+            while(hijo.derecha is not None): hijo = hijo.derecha
+            nodo.dato = hijo.dato
+            self.eliminar(hijo)
+        else:
+            hijo = nodo.derecha
+            while(hijo.izquierda is not None): hijo = hijo.izquierda
+            nodo.dato = hijo.dato
+            self.eliminar(hijo)
+
+    def eliminar(self,dato):
+        if self.raiz is None:
+            return
+        nodo = self.raiz
+        hijo = self.raiz
+        while(hijo is not None):
+            nodo = hijo
+            hijo =  nodo.derecha if dato >= nodo.dato else nodo.izquierda
+            if(dato == nodo.dato):
+                self.eliminar(nodo)
+                return
+
+    def rebalancear(self,nodo):
+        self.estbalance(nodo)
+        if nodo.balance == -2:
+            if(self.altura(nodo.izquierda.izquierda) >= self.altura(nodo.izquierda.derecha)):
+                nodo = self.rotDer(nodo)
+            else:
+                nodo = self.rotIzDer(nodo)
+        elif nodo.balance == 2:
+            if(self.altura(nodo.derecha.derecha) >= self.altura(nodo.derecha.izquierda)):
+                nodo = self.rotIz(nodo)
+            else:
+                nodo = self.rotDerIz(nodo)
+
+        if nodo.padre is not None:
+            self.rebalancear(nodo.padre)
+        else:
+            self.raiz = nodo
+
+    def rotIz(self,nodo):
+        b = nodo.derecha
+        b.padre = nodo.padre
+        nodo.derecha = b.izquierda
+        if nodo.derecha is not None:
+            nodo.derecha.padre = nodo
+        b.izquierda = nodo
+        nodo.padre = b
+        if b.padre is not None:
+            if b.padre.derecha == nodo:
+                b.padre.derecha = b
+            else:
+                b.padre.izquierda = b
+        self.estbalance(nodo,b)
+        return b
+
+    def rotDer(self,nodo):
+        b = nodo.izquierda
+        b.padre = nodo.padre
+        nodo.izquierda = b.derecha
+        if nodo.izquierda is not None:
+            nodo.izquierda.padre = nodo
+        b.derecha = nodo
+        nodo.padre = b
+        if b.padre is not None:
+            if b.padre.derecha == nodo:
+                b.padre.derecha = b
+            else:
+                b.padre.izquierda = b
+        self.estbalance(nodo,b)
+        return b
+
+    def rotIzDer(self,nodo):
+        nodo.izquierda = self.rotIz(nodo.izquierda)
+        return rotDer(nodo)
+
+    def rotDerIz(self,nodo):
+        nodo.derecha = rotDer(nodo.derecha)
+        return rotIz(nodo)
+
+    def altura(self,nodo):
+        if nodo is None:
+            return -1
+        return nodo.altura
+
+    def estbalance(self,*nodos):
+        for n in nodos:
+            self.realt(n)
+            n.balance = self.altura(n.derecha) - self.altura(n.izquierda)
+
+    def realt(self,nodo):
+        if nodo is not None:
+            nodo.altura = 1 + max(self.altura(nodo.izquierda),self.altura(nodo.derecha))
+
+    def graf(self,raiz,dot):
+        if raiz is None:
+            return
+        dot.node(str(raiz.dato))
+        if (raiz.izquierda is not None):
+            dot.node(str(raiz.izquierda.dato))
+            dot.edge(str(raiz.dato),str(raiz.izquierda.dato))
+        if(raiz.derecha is not None):
+            dot.node(str(raiz.derecha.dato))
+            dot.edge(str(raiz.dato),str(raiz.derecha.dato))
+        #contador+=1
+        self.graf(raiz.izquierda,dot)
+        self.graf(raiz.derecha,dot)
+        return dot
+
+    def graficar(self,imagen):
+        dot = Digraph(comment='AVL',format='png')
+        self.graf(self.raiz,dot)
+        dot.render("C:\\Users\\Abraham Jelkmann\\Desktop\\"+imagen,cleanup=True)
+        dot.save(imagen,r"C:\\Users\\Abraham Jelkmann\\Desktop")
+
+"""
+arbol = AVL()
+arbol.insertar(1)
+arbol.insertar(2)
+arbol.insertar(3)
+arbol.insertar(4)
+arbol.insertar(5)
+arbol.insertar(6)
+arbol.insertar(7)
+arbol.insertar(8)
+arbol.insertar(9)
+arbol.graficar('avl')
+"""
--- ArbolB.py
+++ ArbolB.py
@@ -2,7 +2,9 @@
 class ArbolB(object):
     def __init__(self):
         self.raiz = VectorB()
+
     def insertar(self,raiz,dato):
+        #print('Insertando: ' + str(dato))
         if raiz.tiene_hijos():
             actual = raiz.p0
             while dato > actual.dato and actual.siguiente.dato is not None:
@@ -16,46 +18,102 @@ class ArbolB(object):
             raiz.insertar(dato)
             if raiz.cantidad_datos() == 5: # El vector ya está lleno, se debe separar
                 if raiz.padre is not None: # Si tiene padre, se debe insertar el valor medio al padre
-                    if raiz.padre.izquierda = raiz:
-                        print("El padre tiene este hijo a la izquierda")
-                    elif raiz.padre.derecha = raiz:
-                        print("El padre tiene este hijo a la derecha")
-                    else:
-                        print("Mi código no está en nada")
-                    raiz.padre.insertar(raiz.p2.dato)
-                    nod = raiz.padre.buscar(raiz.p2.dato)
 
-                    pass
+                    self.insertararriba(raiz,raiz.padre.vector,raiz.p2.dato)
+
                 else:                       # Si no tiene padre, se crea una nueva raiz
                     vraiz = VectorB()               # Inicializar vraiz
                     vraiz.insertar(raiz.p2.dato) # Insertar el dato medio(p2) en la nueva raiz
+
                     vizquierda = VectorB()            # Crear el hijo izquierdo de la nueva raiz
                     vizquierda.insertar(raiz.p0.dato) # Ingresar los valores del vector izquierdo
                     vizquierda.insertar(raiz.p1.dato)
+
                     vderecha = VectorB()               # Crear el hijo derecho de la nueva raiz
                     vderecha.insertar(raiz.p3.dato)   # Ingresar los valores del vector derecho
                     vderecha.insertar(raiz.p4.dato)
+
                     raiz.cambiar(vraiz)              # Asignar la nueva raiz
-                    raiz.p0.izquierda = vizquierda # Asignar hijos derechos e izquierdos
-                    raiz.p0.derecha = vderecha
-                    vizquierda.padre = raiz.p0 # Asignar padres a los hijos creados
+                    raiz.p0.setIzquierda(vizquierda) # Asignar hijos derechos e izquierdos
+                    raiz.p0.setDerecha(vderecha)
+                    # Asignar padres a los hijos creados
+                    vizquierda.padre = raiz.p0
                     vderecha.padre = raiz.p0
                     # Pruebas, eliminar después
-                    raiz.imprimir()
-                    vizquierda.imprimir()
-                    vderecha.imprimir()
-                    print("La raiz se ha separado")
+                    #raiz.imprimir()
+                    #vizquierda.imprimir()
+                    #vderecha.imprimir()
+                    print("La raiz se ha separado") # raiz es un VectorB
 
+    def insertararriba(self,raiz,padre,dato): # raiz es un VectorB
+
+        padre.insertar(dato) # Agregar el dato al padre
+
+        vizquierda = VectorB() # Crear el vector que será agregado a la izquierda del dato ingresado al padre
+        vizquierda.insertar(raiz.p0.dato)
+        vizquierda.insertar(raiz.p1.dato)
+
+        vderecha = VectorB() # Crear el vector que será agregado a la derecha del dato ingresado al padre
+        vderecha.insertar(raiz.p3.dato)
+        vderecha.insertar(raiz.p4.dato)
+
+        vizquierda.p0.setIzquierda(raiz.p0.izquierda)
+        vizquierda.p1.setIzquierda(raiz.p1.izquierda)
+        vizquierda.p1.setDerecha(raiz.p1.derecha)
+
+        vderecha.p0.setIzquierda(raiz.p3.izquierda)
+        vderecha.p1.setIzquierda(raiz.p4.izquierda)
+        vderecha.p1.setDerecha(raiz.p4.derecha)
+
+        nod = padre.buscar(dato) # nod es el nodo que contiene el dato que ingresamos al inicio al padre
+
+        nod.setIzquierda(vizquierda) # Asignar hijos al dato ingresado
+        nod.setDerecha(vderecha)
+
+        vizquierda.padre = nod
+        vderecha.padre = nod
+
+        if padre.cantidad_datos() == 5:
+            if padre.padre is not None:
+                self.insertararriba(padre,padre.padre.vector,padre.p2.dato)
+            else:
+                vraiz = VectorB()
+                vraiz.insertar(padre.p2.dato)
+
+                vizquierda = VectorB()
+                vizquierda.insertar(padre.p0.dato)
+                vizquierda.insertar(padre.p1.dato)
+
+                vderecha = VectorB()
+                vderecha.insertar(padre.p3.dato)
+                vderecha.insertar(padre.p4.dato)
+
+                vizquierda.p0.setIzquierda(padre.p0.izquierda)
+                vizquierda.p1.setIzquierda(padre.p1.izquierda)
+                vizquierda.p1.setDerecha(padre.p1.derecha)
+
+                vderecha.p0.setIzquierda(padre.p3.izquierda)
+                vderecha.p1.setIzquierda(padre.p4.izquierda)
+                vderecha.p1.setDerecha(padre.p4.derecha)
+
+                padre.cambiar(vraiz)
+
+                padre.p0.setIzquierda(vizquierda)
+                padre.p0.setDerecha(vderecha)
+
+                vizquierda.padre = padre.p0
+                vderecha.padre = padre.p0
 
 class VectorB(object):
-        def __init__(self,size=4):
+    def __init__(self,size=4):
         self.size = size
         #Definir todos los nodos del vector al ser iniciado
-        self.p4 = NodoVector()
-        self.p3 = NodoVector(self.p4)
-        self.p2 = NodoVector(self.p3)
-        self.p1 =  NodoVector(self.p2)
-        self.p0 = NodoVector(self.p1)
+        self.p4 = NodoVector('p4')
+        self.p3 = NodoVector('p3',self.p4,self)
+        self.p2 = NodoVector('p2',self.p3,self)
+        self.p1 =  NodoVector('p1',self.p2,self)
+        self.p0 = NodoVector('p0',self.p1,self)
+        self.p4.vector = self
         #Asignar punteros a los nodos anteriores para simular una lista doble
         #(facilita las inserciones)
         self.p1.anterior = self.p0
@@ -63,12 +121,13 @@ class VectorB(object):
         self.p3.anterior = self.p2
         self.p4.anterior = self.p3
         self.padre=None
+
     def insertar(self,dato):
         insertado = False
         if self.cantidad_datos() == 0:
             self.p0.dato = dato
             insertado = True
-            return print((str(dato) + ' ha sido ingresado'))
+            #return print((str(dato) + ' ha sido ingresado'))
         if self.cantidad_datos() < 5: # Al tener 5 datos se debe separar el vector
             actual = self.p0
             while not insertado:
@@ -77,17 +136,17 @@ class VectorB(object):
                     self.corrernodos(actual)
                     actual.dato = dato
                     insertado = True
-                    return print((str(dato) + ' ha sido ingresado'))
+            #        return print((str(dato) + ' ha sido ingresado'))
                 while (dato > actual.dato):
                     if actual.siguiente.dato is not None:
                         actual = actual.siguiente
                     else:
                         actual.siguiente.dato = dato
                         insertado = True
-                        return print((str(dato) + ' ha sido ingresado'))
+                        #return print((str(dato) + ' ha sido ingresado'))
 #que no quede huella que no y que no
 
-    def corrernodos(self,actual=None):
+    def corrernodos(self,actual=None): #Metodo para desplazar los nodos
         final = self.p4
         while final.dato is None:
             final = final.anterior
@@ -95,18 +154,33 @@ class VectorB(object):
             final.siguiente.dato = final.dato # Correr el dato del nodo una posición a la izquierda
             final.siguiente.izquierda = final.izquierda # Asignar los hijos derechos e izquierdos del nodo una posición a la izquierda
             final.siguiente.derecha = final.derecha
+            if final.izquierda is not None:
+                final.izquierda.padre = final.siguiente # Cambiar los padres de los hijos
+                final.derecha.padre = final.siguiente
+
             final.dato = final.anterior.dato # Correr el dato anterior al nodo actual a la posición del nodo actual
             final.izquierda = final.anterior.izquierda # Asignar los hijos derechos e izquierdos a la posicion del nodo actual
             final.derecha = final.anterior.derecha
+
+            if final.anterior.izquierda is not None:
+                final.anterior.izquierda.padre = final
+                final.anterior.derecha.padre = final
+
             final = final.anterior
         # El código se debe ejecutar una vez más (se puede optimizar pero no quiero pensar)
         final.siguiente.dato = final.dato
         final.siguiente.izquierda = final.izquierda
         final.siguiente.derecha = final.derecha
+        if final.izquierda is not None:
+            final.izquierda.padre = final.siguiente # Cambiar los padres de los hijos
+            final.derecha.padre = final.siguiente
         if final.anterior is not None:
             final.dato = final.anterior.dato
             final.izquierda = final.anterior.izquierda # (Optimizable)
-            final.derecha = final.anterior.derecha #Metodo para desplazar los nodos
+            final.derecha = final.anterior.derecha
+            if final.anterior.izquierda is not None:
+                final.anterior.izquierda.padre = final
+                final.anterior.derecha.padre = final
 
     def cantidad_datos(self): #Retorna la cantidad de datos no nulos en el vector
         cantidad = 0
@@ -124,16 +198,19 @@ class VectorB(object):
         #que no quede huella de ti
 
     def imprimir(self):
+        #print('voy a imprimir')
         actual = self.p0
-        cadena = ''
+        cadena = '| '
         if actual.dato is not None:
+        #    print(actual.dato)
             cadena = cadena + str(actual.dato)
         while(actual.siguiente.dato is not None):
             actual = actual.siguiente
-            cadena = cadena + " _ " + str(actual.dato)
+        #    print(actual.dato)
+            cadena = cadena + " | " + str(actual.dato)
             if actual.siguiente is None:
                 break
-        print(cadena)
+        print('el vector es: ' + cadena + ' |')
 
     def tiene_hijos(self):
         if self.p0.izquierda is not None or self.p0.derecha is not None:
@@ -141,7 +218,7 @@ class VectorB(object):
         else:
             return False
 
-    def cambiar(self,vector):
+    def cambiar(self,vector): #Metodo para cambiar los datos de un vector
         self.p4.dato=None
         self.p3.dato=None
         self.p2.dato=None
@@ -161,19 +238,28 @@ class VectorB(object):
         return ('No se encontró el dato')
 
 
+
 class NodoVector(object):
-    def __init__(self,siguiente=None,anterior=None,dato=None,derecha=None,izquierda=None):
+    def __init__(self,nombre=None,siguiente=None,vector=None,anterior=None,dato=None,derecha=None,izquierda=None):
         self.siguiente=siguiente
         self.anterior=anterior
         self.derecha=derecha
         self.izquierda=izquierda
         self.dato=dato
+        self.vector=vector
+        self.nombre=nombre
 
     def setIzquierda(self,valor):
         self.izquierda = valor
+        if self.anterior is not None:
+            self.anterior.derecha = valor
+
     def setDerecha(self,valor):
         self.derecha = valor
-"""
+        if self.siguiente is not None:
+            self.siguiente.izquierda = valor
+
+""" #pruebas para los vectores
 prueba = VectorB()
 prueba.insertar(50)
 prueba.imprimir()
@@ -192,10 +278,29 @@ arbol.insertar(arbol.raiz,50)
 arbol.insertar(arbol.raiz,5)
 arbol.insertar(arbol.raiz,32)
 arbol.insertar(arbol.raiz,10)
+
+
 arbol.insertar(arbol.raiz,33)
 arbol.insertar(arbol.raiz,3)
 arbol.insertar(arbol.raiz,25)
 arbol.insertar(arbol.raiz,45)
+arbol.insertar(arbol.raiz,8)
+arbol.insertar(arbol.raiz,4)
+arbol.insertar(arbol.raiz,15)
+arbol.insertar(arbol.raiz,46)
+arbol.insertar(arbol.raiz,47)
+arbol.insertar(arbol.raiz,22)
+arbol.insertar(arbol.raiz,17)
+arbol.insertar(arbol.raiz,66)
+arbol.insertar(arbol.raiz,70)
+arbol.insertar(arbol.raiz,80)
+
+"""
 arbol.raiz.imprimir()
 arbol.raiz.p0.izquierda.imprimir()
 arbol.raiz.p0.derecha.imprimir()
+
+arbol.raiz.p1.derecha.imprimir()
+arbol.raiz.p2.derecha.imprimir()
+arbol.raiz.p3.derecha.imprimir()
+"""
